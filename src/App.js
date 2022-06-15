8223d151cb5ae4595c5d384c31a531009c6ce242
Harish Raisinghani <harishraisinghani@gmail.com>
2022-06-15 17:23:45 +0000

    Add github pages deploy workflow

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { WalletBalance } from '@harishraisinghani/wallet-balance-component'
 function App() {
   return(
     <div className="WalletBalance">
-      <WalletBalance apikey={process.env.REACT_APP_COVALENT_API_KEY} />
+      <WalletBalance apikey={process.env.REACT_APP_COVALENT_API} />
     </div>
   )
 }
